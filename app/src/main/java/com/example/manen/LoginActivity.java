0551e3c203c63ea3bf6b46537297b898be9feeb8
GandiYahya <gandiyahya42@gmail.com>
2022-04-26 01:53:12 +0700

    Menambah UI login dan merubah theme suapaya bisa bosa merubah warna tombol

---
+++ app/src/main/java/com/example/manen/LoginActivity.java
@@ -0,0 +1,51 @@
+package com.example.manen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText inputEmail,inputPassword;
+    boolean passwordVisible;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        inputEmail = findViewById(R.id.inputEmailLogin);
+        inputPassword = findViewById(R.id.inputPasswordLogin);
+
+        // show and hide password on password edittext
+        inputPassword.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                final int Right = 2;
+                if (event.getAction()==MotionEvent.ACTION_UP){
+                    if (event.getRawX()>=inputPassword.getRight()-inputPassword.getCompoundDrawables()[Right].getBounds().width()){
+                        int selection=inputPassword.getSelectionEnd();
+                        if(passwordVisible){
+                            inputPassword.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,R.drawable.ic_baseline_visibility_off_24 , 0 );
+                            inputPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                            passwordVisible=false;
+                        }else{
+                            inputPassword.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,R.drawable.ic_baseline_visibility_24 , 0 );
+                            inputPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                            passwordVisible=true;
+                        }
+                        inputPassword.setSelection(selection);
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+        });
+    }
+}
