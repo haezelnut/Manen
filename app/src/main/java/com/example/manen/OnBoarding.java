047ca9f9781f8783420592864230814f971e7ae3
haezelnut <alfinkakurnianto@gmail.com>
2022-04-26 14:56:26 +0700

    penambahan layout verivikasi dan berhasil daftar

--- app/src/main/java/com/example/manen/OnBoarding.java
+++ app/src/main/java/com/example/manen/OnBoarding.java
@@ -3,14 +3,17 @@ package com.example.manen;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class OnBoarding extends AppCompatActivity {
 
     ViewPager viewPager;
     SliderAdapter sliderAdapter;
-
+    Button getStartedBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +22,19 @@ public class OnBoarding extends AppCompatActivity {
         getSupportActionBar().hide();
 
         viewPager = findViewById(R.id.slider);
+        getStartedBtn = findViewById(R.id.getStartedBtn);
 
         //call adapter
         sliderAdapter = new SliderAdapter(this);
 
         viewPager.setAdapter(sliderAdapter);
+
+        getStartedBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(OnBoarding.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
