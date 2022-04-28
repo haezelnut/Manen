4472d63aa4e85008085c1e6bbfa4a456b8a858be
haezelnut <alfinkakurnianto@gmail.com>
2022-04-28 09:38:41 +0700

    menambahkan dots pada onboarding

--- app/src/main/java/com/example/manen/OnBoarding.java
+++ app/src/main/java/com/example/manen/OnBoarding.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 
 public class OnBoarding extends AppCompatActivity {
@@ -22,6 +26,24 @@ public class OnBoarding extends AppCompatActivity {
 
         viewPager = findViewById(R.id.slider);
         getStartedBtn = findViewById(R.id.getStartedBtn);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                dots(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+        viewPager.setAdapter(sliderAdapter);
+        dots(0);
 
         //call adapter
         sliderAdapter = new SliderAdapter(this);
@@ -36,4 +58,29 @@ public class OnBoarding extends AppCompatActivity {
             }
         });
     }
+
+
+    public void dots(int index)
+        {
+            LinearLayout dotsLayout = findViewById(R.id.layoutDot);
+            ImageView[] dots = new ImageView[4];
+
+            dotsLayout.removeAllViews();
+            for (int i = 0;i<dots.length;i++)
+            {
+                dots[i] = new ImageView(this);
+                int width_heigh =25;
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(new ViewGroup.LayoutParams(width_heigh,width_heigh));
+                params.setMargins(10,10,10,10);
+                dots[i].setLayoutParams(params);
+                dots[i].setImageResource(R.drawable.dot);
+                dots[i].setColorFilter(getResources().getColor(R.color.teal_200), PorterDuff.Mode.SRC_IN);
+                dotsLayout.addView(dots[i]);
+            }
+
+            dots[index].setImageResource(R.drawable.dot);
+            dots[index].setColorFilter(getResources().getColor(R.color.black), PorterDuff.Mode.SRC_IN);
+
+        }
+
 }
