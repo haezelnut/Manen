7f0e0b5ee7927df8c7e3d8b3c43ee3bd3c32ad95
haezelnut <alfinkakurnianto@gmail.com>
2022-04-30 15:01:14 +0700

    memperbaiki screen verifikasi yang typo, menambahkan screen pesan dan tambah produk, serta asset yang dibutuhkan

---
+++ app/src/main/java/com/example/manen/TambahProduk.java
@@ -0,0 +1,14 @@
+package com.example.manen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TambahProduk extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tambah_produk);
+    }
+}
