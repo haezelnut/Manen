22bd0996fde74075c648c10c09f99a1075346b88
GandiYahya <gandiyahya42@gmail.com>
2022-05-06 17:32:13 +0700

    Membuat intent bottom nav bar bekerja dan menggunakan fragment pada layar ulasan, chat, pengaturan, dan beranda.

--- app/src/main/java/com/example/manen/HomeActivity.java
+++ app/src/main/java/com/example/manen/HomeActivity.java
@@ -60,38 +60,5 @@ public class HomeActivity extends AppCompatActivity {
            }
        });
 
-
-      /*   bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Fragment temp = null;
-                int id = item.getItemId();
-                if(id == R.id.home){
-                    new HomeFragment();
-
-                }else if (id==R.id.chat){
-                    new PesanFragment();
-                }else if (id==R.id.Reviewer){
-                    new UlasanFragment();
-                }else if (id==R.id.Setting){
-                    new PengaturanFragment();}
-
-                    */
-
-              /*  switch (item.getItemId()){
-                    case R.id.home : new HomeFragment();
-                    break;
-                    case R.id.chat : new PesanFragment();
-                        break;
-                    case R.id.Reviewer: new UlasanFragment();
-                        break;
-                    case R.id.Setting: new PengaturanFragment();
-                }*/
-
-            /*  getSupportFragmentManager().beginTransaction().replace(R.id.main_container,temp).commit();
-
-            return false;
-           }
-      });}*/
     }
 }
